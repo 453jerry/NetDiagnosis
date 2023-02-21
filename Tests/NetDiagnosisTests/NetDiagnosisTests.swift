8471a7d2e90506c633194029d29a613e19dea2d7
Jerry Huang <jerry.huang@jerrynet.xyz>
2023-02-22 01:17:02 +0800

    feat(pinger): implement Pinger and domain name lookup

--- Tests/NetDiagnosisTests/NetDiagnosisTests.swift
+++ Tests/NetDiagnosisTests/NetDiagnosisTests.swift
@@ -1,11 +1,11 @@
-import XCTest
-@testable import NetDiagnosis
-
-final class NetDiagnosisTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(NetDiagnosis().text, "Hello, World!")
-    }
-}
+//import XCTest
+//@testable import NetDiagnosis
+//
+//final class NetDiagnosisTests: XCTestCase {
+//    func testExample() throws {
+//        // This is an example of a functional test case.
+//        // Use XCTAssert and related functions to verify your tests produce the correct
+//        // results.
+//        XCTAssertEqual(NetDiagnosis().text, "Hello, World!")
+//    }
+//}
