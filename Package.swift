178ecfc82991538b2ebb6a518c56f79ece6ea347
Jerry Huang <15866475+453jerry@users.noreply.github.com>
2024-02-25 16:43:20 +0800

    build(package): modify minimum platform version to 5.0 for watchos

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "NetDiagnosis",
-    platforms: [.iOS(.v9), .macOS(.v10_10), .watchOS(.v3), .tvOS(.v9)], //As RxSwfit Requiredb
+    platforms: [.iOS(.v9), .macOS(.v10_10), .watchOS(.v5), .tvOS(.v9)], //As RxSwfit Requiredb
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
